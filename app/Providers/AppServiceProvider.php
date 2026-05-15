1311fde29b9cff36fbc6b93936fa64cc67e3980b
nylanisya <naylaanisya7@gmail.com>
2026-05-16 01:42:29 +0700

    fix: paksa https untuk form security

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- 1. PASTIKAN BARIS INI ADA
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 2. TAMBAHKAN KODE INI DI DALAM BOOT
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
